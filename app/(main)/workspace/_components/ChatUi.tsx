1b062d3e457d0b4f33c65f784478720a99908430
Anas Amin <anas214fts@gmail.com>
2026-01-07 10:09:58 +0500

    updated the api endpoint

--- app/(main)/workspace/_components/ChatUi.tsx
+++ app/(main)/workspace/_components/ChatUi.tsx
@@ -214,7 +214,7 @@ const ChatUi = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post("/api/google-gemini", {
+      const response = await axios.post("/api/chat", {
         userInput: input,
         userInstruction:
           assistant?.userInstruction +
